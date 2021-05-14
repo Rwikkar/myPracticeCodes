16c211cb77b1f29efced313c7051edf5c8663e4a
Rwikkar <karsoubhik8@gmail.com>
2021-05-14 21:23:37 +0530

    added new C++ file

---
+++ C++/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class student {
+	public:
+		int a;
+		string n;
+};
+
+int main(){
+	
+	student student1;
+	student1.a=1;
+	student1.n="Soubhik";
+	cout << student1.a << "\n";
+	cout << student1.n;
+	
+	
+}
